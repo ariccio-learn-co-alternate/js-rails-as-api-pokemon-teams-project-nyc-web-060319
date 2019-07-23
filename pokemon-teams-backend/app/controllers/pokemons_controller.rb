2b2a68295f1db8541469907a7cd590e453d9829f
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-23 12:45:33 -0400

    something is working

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,11 +1,15 @@
 class PokemonsController < ApplicationController
-    def index
-        render json: Pokemon.all
-    end
+  def index
+    render json: Pokemon.all
+  end
 
-    def show
-        # trainer_id = params[:id]
-        pokemons = Pokemon.all.where(trainer_id: params[:id])
-        render json: pokemons
-    end
+  def show
+    # trainer_id = params[:id]
+    pokemons = Pokemon.all.where(id: params[:id])
+    byebug
+    render json: pokemons
+  end
+
+  def update
+  end
 end
--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,7 +1,13 @@
 class TrainersController < ApplicationController
-    def index
-        trainers = Trainer.all
-        render json: trainers
-    end
+  def index
+    trainers = Trainer.all
+    render json: trainers
+  end
 
+  def show
+    pokemon = Pokemon.where(trainer_id: params[:id])
+
+    render json: pokemon
+    byebug
+  end
 end
